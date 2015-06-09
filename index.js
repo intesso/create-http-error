21624b0f1ddbc0bbe8288f03eff19197b2da2066
andi-neck <andi.neck@intesso.com>
2015-06-09 17:00:40 +0200

    initial

---
+++ index.js
@@ -0,0 +1,79 @@
+/**
+ * module dependencies.
+ */
+var codes = require('http').STATUS_CODES;
+
+/**
+ * HttpError constructor function.
+ *
+ * @param code http error code
+ * @param message optional message
+ * @returns {HttpError}
+ * @constructor
+ */
+function HttpError(code, message, properties) {
+  // make sure calling HttpError also works without the new keyword
+  if (!(this instanceof HttpError)) return new HttpError(code, message);
+
+  // default arguments
+  code = code || 500;
+  code = typeof code === 'number' ? code : Number(code);
+  message = message || codes[code] || codes[500];
+
+  // create Error
+  Error.call(this, message);
+  this.name = 'HttpError';
+  this.code = this.status = this.statusCode = code;
+  this.msg = message; // message property is not assignable with `Error` Object. therefore use `msg` property
+
+  var self = this;
+  if (properties) {
+    Object.keys(properties).forEach(function() {
+
+    });
+  }
+
+  Error.captureStackTrace(this, HttpError);
+}
+
+/**
+ * inherit from `Error`.
+ *
+ * @type {Error}
+ */
+HttpError.prototype = Object.create(Error.prototype, {
+  constructor: {value: HttpError}
+});
+
+/**
+ * return the `Error`s properties.
+ *
+ * @returns `Error` properties.
+ */
+HttpError.prototype.toJSON = function() {
+  var self = this;
+  var obj = {};
+  Object.keys(this).forEach(function(key) {
+    obj[key] = self[key];
+  });
+
+  return obj;
+};
+
+HttpError.prototype.toString = function() {
+  var msg = this.msg || '';
+  return 'HttpError: ' + msg;
+};
+
+
+/**
+ * Expose HttpError
+ * @type {HttpError}
+ */
+exports = module.exports = HttpError;
+
+/**
+ * Expose http codes
+ * @type {exports.STATUS_CODES|*}
+ */
+exports.codes = codes;
---
+++ test.js
@@ -0,0 +1,53 @@
+var test = require('tape');
+var HttpError = require('./index');
+
+test('create error with code', function(t) {
+  var err = HttpError(500);
+  t.ok(err);
+  t.equal(err.code, 500);
+  t.equal(err.status, 500);
+  t.equal(err.statusCode, 500);
+  t.equal(err.msg, 'Internal Server Error');
+  t.equal(err.name, 'HttpError');
+
+  t.end();
+});
+
+test('create error with specific message', function(t) {
+  var err = HttpError('404', 'uups');
+  t.ok(err);
+  t.equal(err.status, 404);
+  t.equal(err.msg, 'uups');
+  t.equal(err.name, 'HttpError');
+
+  t.end();
+});
+
+test('test codes', function(t) {
+
+  var codes = HttpError.codes;
+  t.ok(codes);
+  t.equal(codes[200], 'OK');
+  t.equal(codes[400], 'Bad Request');
+  t.end();
+});
+
+test('test toString()', function(t) {
+
+  var err = HttpError('400');
+  var str = err.toString();
+  t.ok(str);
+
+  t.equal(str, 'HttpError: Bad Request');
+  t.end();
+});
+
+test('test toJSON()', function(t) {
+
+  var err = HttpError('400');
+  var json = err.toJSON();
+  t.ok(json);
+
+  t.equal(json.msg, 'Bad Request');
+  t.end();
+});
